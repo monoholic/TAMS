29602089cd34cd1b6a3522d395c921df0f263c52
taku <taku@DESKTOP-91DMTIF.mshome.net>
2021-11-04 17:33:23 +0900

    샘플 자료 페이지 추가.

--- TAMS/src/main/java/kr/co/trito/tams/web/sample/controller/SampleController.java
+++ TAMS/src/main/java/kr/co/trito/tams/web/sample/controller/SampleController.java
@@ -65,4 +65,154 @@ public class SampleController {
 	public String screen() {
 		return "/sample/00-화면구성";
 	}
+	
+	@GetMapping(value = "/list1")
+	public String list1() {
+		return "/sample/01-검색+리스트_투자정보조회";
+	}
+	
+	@GetMapping(value = "/list2")
+	public String list2() {
+		return "/sample/01-검색+리스트_팝업";
+	}
+	
+	@GetMapping(value = "/list3")
+	public String list3() {
+		return "/sample/01-입력+리스트_재활용_재활용의뢰작성";
+	}
+	
+	@GetMapping(value = "/list4")
+	public String list4() {
+		return "/sample/01-입력+리스트_팝업-일괄업로드";
+	}
+	
+	@GetMapping(value = "/list5")
+	public String list5() {
+		return "/sample/03-검색+리스트";
+	}
+	
+	@GetMapping(value = "/list6")
+	public String list6() {
+		return "/sample/04-탭+검색+리스트_소계";
+	}
+	
+	@GetMapping(value = "/list7")
+	public String list7() {
+		return "/sample/06-탭+검색+리스트_버튼_선택_입력";
+	}
+	
+	@GetMapping(value = "/list8")
+	public String list8() {
+		return "/sample/08-리스트안내문구";
+	}
+	
+	@GetMapping(value = "/list9")
+	public String list9() {
+		return "/sample/17-리스트헤더유지";
+	}
+	
+	@GetMapping(value = "/regist1")
+	public String regist1() {
+		return "/sample/01-조회+등록_신규자산등록";
+	}
+	
+	@GetMapping(value = "/regist2")
+	public String regist2() {
+		return "/sample/01-조회+리스트_등록자산목록";
+	}
+	
+	@GetMapping(value = "/tab1")
+	public String tab1() {
+		return "/sample/01-탭+검색";
+	}
+	
+	@GetMapping(value = "/tab2")
+	public String tab2() {
+		return "/sample/01-탭+상세_변경이력";
+	}
+	
+	@GetMapping(value = "/tab3")
+	public String tab3() {
+		return "/sample/01-탭+상세_자산정보탭";
+	}
+	
+	@GetMapping(value = "/popup1")
+	public String popup1() {
+		return "/sample/02-메세지창_모달";
+	}
+	
+	@GetMapping(value = "/popup2")
+	public String popup2() {
+		return "/sample/05-팝업_모달";
+	}
+	
+	@GetMapping(value = "/popup3")
+	public String popup3() {
+		return "/sample/02-메세지창_팝업_경고";
+	}
+	
+	@GetMapping(value = "/popup4")
+	public String popup4() {
+		return "/sample/02-메세지창_팝업_에러";
+	}
+	
+	@GetMapping(value = "/popup5")
+	public String popup5() {
+		return "/sample/02-메세지창_팝업_확인";
+	}
+	
+	@GetMapping(value = "/popup6")
+	public String popup6() {
+		return "/sample/07-팝업호출버튼";
+	}
+	
+	@GetMapping(value = "/popup7")
+	public String popup7() {
+		return "/sample/10-팝업뷰";
+	}
+	
+	@GetMapping(value = "/popup8")
+	public String popup8() {
+		return "/sample/11-팝업이미지뷰";
+	}
+	
+	@GetMapping(value = "/btn1")
+	public String btn1() {
+		return "/sample/09-선택박스(다중)";
+	}
+	
+	@GetMapping(value = "/btn2")
+	public String btn2() {
+		return "/sample/15-비활성버튼";
+	}
+	
+	@GetMapping(value = "/btn3")
+	public String btn3() {
+		return "/sample/16-공통버튼_라디오버튼";
+	}
+	
+	@GetMapping(value = "/board1")
+	public String board1() {
+		return "/sample/19-게시판 등록";
+	}
+	
+	@GetMapping(value = "/board2")
+	public String board2() {
+		return "/sample/20-게시판 상세";
+	}
+	
+	@GetMapping(value = "/etc1")
+	public String etc1() {
+		return "/sample/12-로딩";
+	}
+	
+	@GetMapping(value = "/etc2")
+	public String etc2() {
+		return "/sample/13-메일폼";
+	}
+	
+	@GetMapping(value = "/etc3")
+	public String etc3() {
+		return "/sample/18-달력";
+	}
 }
