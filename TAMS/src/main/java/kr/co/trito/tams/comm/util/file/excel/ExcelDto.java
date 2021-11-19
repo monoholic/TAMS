898254e7ef3756b5eec42e0a9170dbbd62e8e459
Suridosa <Suridosa@DESKTOP-D93M826>
2021-11-19 18:13:28 +0900

    @ApiModel 정보추가

--- TAMS/src/main/java/kr/co/trito/tams/comm/util/file/excel/ExcelDto.java
+++ TAMS/src/main/java/kr/co/trito/tams/comm/util/file/excel/ExcelDto.java
@@ -2,16 +2,23 @@ package kr.co.trito.tams.comm.util.file.excel;
 
 import org.apache.poi.ss.usermodel.Row;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@ApiModel("투자정보 엑셀업로드용 Dto")
 public abstract class ExcelDto {
 	
+	@ApiModelProperty(value="등록자")
 	private String regr;
+	@ApiModelProperty(value="등록일자")
 	private String updr;
+	@ApiModelProperty(value="수정자")
 	private String regDt;
+	@ApiModelProperty(value="수정일자")
 	private String upDt;
 
 	public abstract ExcelDto row(Row row);
