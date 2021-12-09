23fbe9a0291e68b941a66a7edbec1d5854a6db74
Tommy <Tommy@host.docker.internal>
2021-12-09 10:28:00 +0900

    Docket 등록

--- TAMS/src/main/java/kr/co/trito/tams/comm/config/SwaggerConfig.java
+++ TAMS/src/main/java/kr/co/trito/tams/comm/config/SwaggerConfig.java
@@ -17,32 +17,71 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	private final String title = "TAMS(Trito Asset Management System)";
+	private final String version = "1.0";
+	private final String apiBasePackage = "kr.co.trito.tams";
 
-    private final String apiBasePackage = "kr.co.trito.tams";
-    private final String apiDescription = "Trito TAMS API Docs";
-    private String version;
-    private String title;
-        
-    @Bean
-    public Docket api() {
-    	version = "Api";
-    	title = "Trito TAMS";
-    	
-        return new Docket(DocumentationType.SWAGGER_2)
-        		.useDefaultResponseMessages(false)
-        		.groupName(version)
-                .select()        		
-                .apis(RequestHandlerSelectors.basePackage(apiBasePackage))
-                .paths(PathSelectors.any())
-                .build()
-        		.apiInfo(apiInfo(title, version));
-    }
+	private String apiDescription = "Trito TAMS API Docs";
+	private String groupName = "";
 
-    public ApiInfo apiInfo(String title, String version) {
-		return new ApiInfoBuilder()
-			.title(title)
-			.version(version)
-			.description(apiDescription)
-			.build();
-    }    
+	@Bean
+	public Docket common() {
+		groupName = "Common";
+		apiDescription = "공통모듈 Docs";
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.groupName(groupName)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(apiBasePackage.concat(".comm")))
+				.paths(PathSelectors.any())
+				.build().apiInfo(apiInfo(title, version));
+	}
+	
+	 
+	@Bean
+	public Docket asset() {
+		groupName = "Asset";
+		apiDescription = "자산모듈 Docs";
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.groupName(groupName)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(apiBasePackage.concat(".web.asset")))
+				.paths(PathSelectors.any())
+				.build().apiInfo(apiInfo(title, version));
+	}	
+
+	@Bean
+	public Docket standard() {
+		groupName = "Standard";
+		apiDescription = "기준정보 Docs";
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.groupName(groupName)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(apiBasePackage.concat(".web.standard")))
+				.paths(PathSelectors.any())
+				.build().apiInfo(apiInfo(title, version));
+	}		
+	
+	@Bean
+	public Docket system() {
+		groupName = "System";
+		apiDescription = "시스쳄관리 Docs";
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.groupName(groupName)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(apiBasePackage.concat(".web.system")))
+				.paths(PathSelectors.any())
+				.build().apiInfo(apiInfo(title, version));
+	}		
+	
+	public ApiInfo apiInfo(String title, String version) {
+		return new ApiInfoBuilder().title(title).version(version).description(apiDescription).build();
+	}
 }
