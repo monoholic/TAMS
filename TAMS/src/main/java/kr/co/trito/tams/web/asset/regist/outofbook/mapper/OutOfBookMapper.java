fffd801e7e82b70f2f13687471e7efb416d08878
Tommy <Tommy@host.docker.internal>
2021-12-29 15:35:13 +0900

    신규 파일 생성

--- TAMS/src/main/java/kr/co/trito/tams/web/asset/regist/outofbook/mapper/OutOfBookMapper.java
+++ TAMS/src/main/java/kr/co/trito/tams/web/asset/regist/outofbook/mapper/OutOfBookMapper.java
@@ -8,7 +8,8 @@ import kr.co.trito.tams.comm.util.search.SearchCondition;
 import kr.co.trito.tams.web.asset.regist.outofbook.dto.AssetMasDto;
 
 @Mapper
-public interface OutOfBookMapper {	
+public interface OutOfBookMapper {
+	
 	public int selectOutOfBookCount(SearchCondition condition);	
 	public List<AssetMasDto> selectOutOfBookList(SearchCondition condition);
 }
